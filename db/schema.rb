fbc932b9adcfadc3d8f9ac0d4fd20de540971717
Graham Noel <grahamnoel@gmail.com>
2019-04-11 19:35:09 +0100

    Scaffold Categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_181211) do
+ActiveRecord::Schema.define(version: 2019_04_11_183159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "retailers", force: :cascade do |t|
     t.string "name"
