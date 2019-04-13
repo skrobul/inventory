809bc9730f7ec2e3f621ac809072d08852be3e89
Graham Noel <grahamnoel@gmail.com>
2019-04-13 11:58:13 +0100

    Add date column to items + purchases

--- db/migrate/20190412103101_create_items.rb
+++ db/migrate/20190412103101_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.date :date
       t.string :name
       t.references :category, foreign_key: true
     end
--- db/migrate/20190412150758_create_purchases.rb
+++ db/migrate/20190412150758_create_purchases.rb
@@ -1,6 +1,7 @@
 class CreatePurchases < ActiveRecord::Migration[5.2]
   def change
     create_table :purchases do |t|
+      t.date :date
       t.references :retailer, foreign_key: true
       t.references :item, foreign_key: true
       t.integer :quantity
