ac477b2cd79a8b9da2cf54e7b94ee6db27b30ee9
Graham Noel <grahamnoel@gmail.com>
2019-04-13 12:14:46 +0100

    Item resource does not need a date

--- db/migrate/20190412103101_create_items.rb
+++ db/migrate/20190412103101_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.date :date
       t.string :name
       t.references :category, foreign_key: true
     end
